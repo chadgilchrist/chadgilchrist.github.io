549bc6cd9f9e0420b7b3734018a4ae5354aed775
Chad Gilchrist <42161763+chadgilchrist@users.noreply.github.com>
2022-06-14 07:16:33 +1200

    api

--- million-dollar-idea/src/app/app.module.ts
+++ million-dollar-idea/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { SharedService } from './shared.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
